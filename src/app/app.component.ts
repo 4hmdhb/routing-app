a6f008372b304a03d4a418b7faff331c3c790035
4hmdhb <4hmdhm@berkeley.edu>
2025-03-16 09:36:31 -0700

    short

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'routing-app';
+}
+
+
+@Input()
+set id(heroId: string) {
+  this.hero$ = this.service.getHero(heroId);
 }
